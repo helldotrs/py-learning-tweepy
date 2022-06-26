2d29fae799446dc915cf850934f3ee6671b38015
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-27 00:58:10 +0200

    Create hello-tweepy.py

---
+++ hello-tweepy.py
@@ -0,0 +1,13 @@
+import tweepy
+
+auth = tweepy.OAuth1UserHandler(
+   consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+    
+#source: https://docs.tweepy.org/en/stable/getting_started.html
